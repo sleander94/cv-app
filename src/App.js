90d142e32ccd3173f43d27cd22616c32d50f52bc
Stephen Leander <stephen.leander1@gmail.com>
2022-05-30 11:14:05 +0200

    Add personal state with handleChange function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Form from './components/Form';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      personal: {
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        description: '',
+      },
+    };
+    this.handleChange.bind(this);
   }
+
+  handleChange = (e) => {
+    this.setState({ personal: { name: e.target.value } });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>CV - Generator</h1>
-        <Form {...this.state} />
+        <Form {...this.state} handleChange={this.handleChange} />
       </div>
     );
   }
