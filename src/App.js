19a8a62d3ac90e940b61afefaa3725f8c9b4781a
Stephen Leander <stephen.leander1@gmail.com>
2022-05-30 11:13:22 +0200

    Import Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App">test</div>;
+import React, { Component } from 'react';
+import Form from './components/Form';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>CV - Generator</h1>
+        <Form {...this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
